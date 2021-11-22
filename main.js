6fe65d237fd3902d105da00e01f9b962e60483e8
Valkyrux <philadephos.granata@gmail.com>
2021-11-22 15:57:19 +0100

    hamburger.close

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 // selezioniamo l'hamburger menu e l'icona con cui attivarlo all'interno del DOM
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const hamburgerMenuIcon = document.querySelector(".header-right ul ~ a");
-// usiamo il metodo .addEventListener per raccogliere l'input dell'utente
+// usiamo il metodo .addEventListener per raccogliere l'input dell'utente e aggiungere la classe active ad hamburger-menu
 hamburgerMenuIcon.addEventListener("click", 
     function() {
         hamburgerMenu.classList.add("active");
     } 
+)
+// individuiamo l'icona per chiudere all'interno del DOM
+const closeHamburger = document.querySelector(".close");
+// agli eventi click togliamo la classe active da hamburger-menu
+closeHamburger.addEventListener("click",
+    function() {
+        hamburgerMenu.classList.remove("active");
+    }    
 )
